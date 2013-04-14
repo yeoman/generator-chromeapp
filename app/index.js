50162f0b481921d09ab872c61f3815b02746e6d3
Sindre Sorhus <sindresorhus@gmail.com>
2013-04-14 02:47:20 +0200

    Code style

--- app/index.js
+++ app/index.js
@@ -1,19 +1,18 @@
 var path = require('path');
 var util = require('util');
-var yeoman = require('yeoman-generators');
+var yeoman = require('yeoman-generator');
 
 module.exports = Generator;
 
 function Generator() {
-  yeoman.generators.Base.apply( this, arguments );
-  this.sourceRoot( path.join( __dirname, 'templates' ) );
-  this.appPermissions = {
-  };
+  yeoman.generators.Base.apply(this, arguments);
+  this.sourceRoot(path.join(__dirname, 'templates'));
+  this.appPermissions = {};
 }
 
-util.inherits( Generator, yeoman.generators.NamedBase );
+util.inherits(Generator, yeoman.generators.NamedBase);
 
-Generator.prototype.askFor = function askFor( argument ) {
+Generator.prototype.askFor = function askFor(argument) {
   var cb = this.async();
 
   var prompts = [{
@@ -21,94 +20,80 @@ Generator.prototype.askFor = function askFor( argument ) {
     message: 'What would you like to call this application?',
     default: 'myChromeApp',
     warning: 'You can change the default application name.'
-  },
-  {
+  }, {
     name: 'appDescription',
     message: 'How would you like to describe this application?',
     default: 'My Chrome app',
     warning: 'You can change the default app description.'
-  },
-  {
+  }, {
     name: 'unlimtedStoragePermission',
     message: 'Would you like to use Storage in your app',
-    default: "Y/n",
+    default: 'Y/n',
     warning: 'You can change the permissions'
-  },
-  {
+  }, {
     name: 'identityPermission',
     message: 'Would you like to the experimental Identity API in your app',
-    default: "Y/n",
+    default: 'Y/n',
     warning: 'You can change the permissions'
-  },
-  {
+  }, {
     name: 'webviewPermission',
     message: 'Would you like to use the webview in your app',
-    default: "Y/n",
+    default: 'Y/n',
     warning: 'You can change the permissions'
-  },
-  {
+  }, {
     name: 'videoCapturePermission',
     message: 'Would you like to use the Camera in your app',
-    default: "Y/n",
+    default: 'Y/n',
     warning: 'You can change the permissions'
-  },
-  {
+  }, {
     name: 'audioCapturePermission',
     message: 'Would you like to use the Microphone in your app',
-    default: "Y/n",
+    default: 'Y/n',
     warning: 'You can change the permissions'
-  },
-  {
+  }, {
     name: 'usbPermission',
     message: 'Would you like to use USB in your app',
-    default: "Y/n",
+    default: 'Y/n',
     warning: 'You can change the permissions'
-  },
-  {
+  }, {
     name: 'bluetoothPermission',
     message: 'Would you like to use Bluetooth in your app',
-    default: "Y/n",
+    default: 'Y/n',
     warning: 'You can change the permissions'
-  },
-  {
+  }, {
     name: 'serialPermission',
     message: 'Would you like to use the Serial Port in your app',
-    default: "Y/n",
+    default: 'Y/n',
     warning: 'You can change the permissions'
-  },
-  {
+  }, {
     name: 'udpsendPermission',
     message: 'Would you like to send UDP data in your app',
-    default: "Y/n",
+    default: 'Y/n',
     warning: 'You can change the permissions'
-  },
-  {
+  }, {
     name: 'udpbindPermission',
     message: 'Would you like to receive UDP data in your app',
-    default: "Y/n",
+    default: 'Y/n',
     warning: 'You can change the permissions'
-  },
-  {
+  }, {
     name: 'tcpPermission',
     message: 'Would you like to use TCP in your app?',
-    default: "Y/n",
+    default: 'Y/n',
     warning: 'You can change the permissions'
-  },
-  {
+  }, {
     name: 'mediagalleryPermission',
     message: 'Would you like to use the Media Gallery API in your app?',
-    default: "Y/n",
+    default: 'Y/n',
     warning: 'You can change the permissions'
-  }
+  }];
 
-  ];
-
-  this.prompt( prompts, function( err, props ) {
-    if ( err ) {
+  this.prompt(prompts, function (err, props) {
+    if (err) {
       return this.emit( 'error', err );
     }
+
     this.appFullName = props.appFullName;
-    this.appDescription = props.appDescription
+    this.appDescription = props.appDescription;
     this.appPermissions.serial = (/y/i).test(props.serialPermission);
     this.appPermissions.identity = (/y/i).test(props.identityPermission);
     this.appPermissions.unlimitedStorage = (/y/i).test(props.unlimitedStoragePermission);
@@ -119,54 +104,67 @@ Generator.prototype.askFor = function askFor( argument ) {
     this.appPermissions.videoCapture = (/y/i).test(props.videoCapturePermission);
 
     var connections = [];
-    if((/y/i).test(props.udpbindPermission)) connections.push("udp-bind::8899");
-    if((/y/i).test(props.udpsendPermission)) connections.push("udp-send-to::8899");
-    if((/y/i).test(props.tcpPermission)) connections.push("tcp-connect");
 
+    if ((/y/i).test(props.udpbindPermission)) {
+      connections.push('udp-bind::8899');
+    }
+
+    if ((/y/i).test(props.udpsendPermission)) {
+      connections.push('udp-send-to::8899');
+    }
+
+    if ((/y/i).test(props.tcpPermission)) {
+      connections.push('tcp-connect');
+    }
 
     // Complex permission objects
-    if((/y/i).test(props.mediagalleryPermission))
-      this.appPermissions.mediaGalleries = { "mediaGalleries" : ["read", "allAutoDetected"] };
+    if ((/y/i).test(props.mediagalleryPermission))
+      this.appPermissions.mediaGalleries = { 'mediaGalleries': ['read', 'allAutoDetected'] };
 
-    if(connections.length > 0)
+    if (connections.length > 0) {
       this.appPermissions.socket = { 'socket': connections };
+    }
 
     cb();
   }.bind( this ));
 };
 
-Generator.prototype.writeFiles = function() {
+Generator.prototype.writeFiles = function () {
   var data = this.buildData();
 
-  this.directory( '.', '.' );
-
-  this.template( 'app/index.html', path.join( 'app', 'index.html' ), data );
-  this.template( 'app/manifest.json', path.join( 'app', 'manifest.json' ), data );
-  this.template( 'app/_locales/en/messages.json', path.join( 'app', '_locales', 'en' , 'messages.json' ), data );
+  this.directory('.', '.');
+  this.template('app/index.html', path.join('app', 'index.html'), data);
+  this.template('app/manifest.json', path.join('app', 'manifest.json'), data);
+  this.template('app/_locales/en/messages.json', path.join('app', '_locales', 'en', 'messages.json' ), data);
 }
 
 Generator.prototype.buildData = function () {
-
   var experimental = {
-    "bluetooth" : true,
-    "usb": true,
-    "identity": true
+    'bluetooth' : true,
+    'usb': true,
+    'identity': true
   };
 
   // Using object to maintain complex objects rather than strings.
   var complex = {
-    "socket": true,
-    "mediaGalleries": true
+    'socket': true,
+    'mediaGalleries': true
   };
 
   var permissions = [];
   var usesExperimental = false;
   var complexPermissions = [];
 
-  for(var permission in this.appPermissions) {
-    if(!!this.appPermissions[permission] == false) continue;
-    if(experimental[permission]) usesExperimental = true;
-    if(complex[permission])  {
+  for (var permission in this.appPermissions) {
+    if (!!this.appPermissions[permission] === false) {
+      continue;
+    }
+
+    if (experimental[permission]) {
+      usesExperimental = true;
+    }
+
+    if (complex[permission]) {
       complexPermissions.push(this.appPermissions[permission]);
       continue;
     }
@@ -174,15 +172,18 @@ Generator.prototype.buildData = function () {
     permissions.push(permission);
   }
 
-  if(usesExperimental) permissions.push("experimental");
+  if (usesExperimental) {
+    permissions.push('experimental');
+  }
+
   var data = {
     appFullName: this.appFullName,
     appDescription: this.appDescription,
     appPermissions: permissions
   };
 
-  if(complexPermissions.length > 0) {
-    for(var p = 0; permission = complexPermissions[p]; p ++) {
+  if (complexPermissions.length > 0) {
+    for (var p = 0; permission = complexPermissions[p]; p ++) {
       // Complex permissions aren't keyed off the name, remove it.
       delete data.appPermissions[permission];
       data.appPermissions.push(permission);
--- test/test-renderfile.js
+++ test/test-renderfile.js
@@ -1,178 +1,178 @@
-var assert = require("assert");
-var Generator = require("../all");
-var grunt = require("grunt");
-var path = require("path");
+/*global describe, it */
+'use strict';
+var assert = require('assert');
+var path = require('path');
+var yeoman = require('yeoman-generator');
+var Generator = require('../app');
 
 var render = function(source, data) {
   return grunt.template.process(grunt.file.read(path.join('all', 'templates', 'app', source)), data);
 };
 
-
-describe("Generator", function() {
-  describe("#buildData", function() {
-    it("should contain no permissions when none asked for", function() {
+describe('Generator', function () {
+  describe('#buildData', function () {
+    it('should contain no permissions when none asked for', function () {
        var g = new Generator();
 
        var manifest = g.buildData();
        assert.equal(0, manifest.appPermissions.length);
     });
 
-    it("should populate appFullName when user provides a name", function() {
+    it('should populate appFullName when user provides a name', function () {
        var g = new Generator();
-       g.appFullName = "test1234";
+       g.appFullName = 'test1234';
 
        var manifest = g.buildData();
-       assert.equal("test1234", manifest.appFullName);
+       assert.equal('test1234', manifest.appFullName);
     });
 
-    it("should populate appDescription when user provides a description", function() {
+    it('should populate appDescription when user provides a description', function () {
        var g = new Generator();
-       g.appDescription = "test1234";
+       g.appDescription = 'test1234';
 
        var manifest = g.buildData();
-       assert.equal("test1234", manifest.appDescription);
+       assert.equal('test1234', manifest.appDescription);
     });
 
-    it("should set unlimitedStoragePermission when developer wants unlimitedStorage", function() {
+    it('should set unlimitedStoragePermission when developer wants unlimitedStorage', function () {
        var g = new Generator();
        g.appPermissions = {};
        g.appPermissions.unlimitedStorage = true;
 
        var manifest = g.buildData();
        var manifestObj = manifest.appPermissions;
-       assert.equal(0, manifestObj.indexOf("unlimitedStorage"));
+       assert.equal(0, manifestObj.indexOf('unlimitedStorage'));
        assert.equal(1, manifestObj.length);
     });
 
-    it("should set identity permission when developer wants identity", function() {
+    it('should set identity permission when developer wants identity', function () {
        var g = new Generator();
        g.appPermissions = {};
        g.appPermissions.identity = true;
 
        var manifest = g.buildData();
        var manifestObj = manifest.appPermissions;
-       assert.notEqual(-1, manifestObj.indexOf("identity"));
-       assert.notEqual(-1, manifestObj.indexOf("experimental"));
+       assert.notEqual(-1, manifestObj.indexOf('identity'));
+       assert.notEqual(-1, manifestObj.indexOf('experimental'));
        assert.equal(2, manifestObj.length);
     });
 
-    it("should not set identity permission when developer doesn't want identity", function() {
+    it('should not set identity permission when developer doesn\'t want identity', function () {
        var g = new Generator();
        g.appPermissions = {};
        g.appPermissions.identity = false;
 
        var manifest = g.buildData();
        var manifestObj = manifest.appPermissions;
-       assert.equal(-1, manifestObj.indexOf("identity"));
-       assert.equal(-1, manifestObj.indexOf("experimental"));
+       assert.equal(-1, manifestObj.indexOf('identity'));
+       assert.equal(-1, manifestObj.indexOf('experimental'));
        assert.equal(0, manifestObj.length);
     });
 
-    it("should set usb permission when developer wants usb", function() {
+    it('should set usb permission when developer wants usb', function () {
        var g = new Generator();
        g.appPermissions = {};
        g.appPermissions.usb = true;
 
        var manifest = g.buildData();
        var manifestObj = manifest.appPermissions;
-       assert.notEqual(-1, manifestObj.indexOf("usb"));
-       assert.notEqual(-1, manifestObj.indexOf("experimental"));
+       assert.notEqual(-1, manifestObj.indexOf('usb'));
+       assert.notEqual(-1, manifestObj.indexOf('experimental'));
        assert.equal(2, manifestObj.length);
     });
 
-    it("should not set usb permission when developer doesn't want usb", function() {
+    it('should not set usb permission when developer doesn\'t want usb', function () {
        var g = new Generator();
        g.appPermissions = {};
        g.appPermissions.usb = false;
 
        var manifest = g.buildData();
        var manifestObj = manifest.appPermissions;
-       assert.equal(-1, manifestObj.indexOf("usb"));
-       assert.equal(-1, manifestObj.indexOf("experimental"));
+       assert.equal(-1, manifestObj.indexOf('usb'));
+       assert.equal(-1, manifestObj.indexOf('experimental'));
        assert.equal(0, manifestObj.length);
     });
 
-   it("should set mediaGalleries permission when developer wants mediaGalleries API", function() {
+   it('should set mediaGalleries permission when developer wants mediaGalleries API', function () {
        var g = new Generator();
        g.appPermissions = {};
 
        /// turns out we can't set the params so we need to assume that params parser is correct
-       g.appPermissions.mediaGalleries = { "mediaGalleries" : ["read", "allAutoDetected"] };
+       g.appPermissions.mediaGalleries = { 'mediaGalleries': ['read', 'allAutoDetected'] };
 
        var manifest = g.buildData();
        var manifestObj = manifest.appPermissions;
        assert.notEqual(false, !!manifestObj[0].mediaGalleries);
-       assert.notEqual(-1, manifestObj[0].mediaGalleries.indexOf("read"));
-       assert.notEqual(-1, manifestObj[0].mediaGalleries.indexOf("allAutoDetected"));
+       assert.notEqual(-1, manifestObj[0].mediaGalleries.indexOf('read'));
+       assert.notEqual(-1, manifestObj[0].mediaGalleries.indexOf('allAutoDetected'));
        assert.equal(1, manifestObj.length);
     });
   });
 
-  describe("#createManifest", function() {
-    it("should have an empty permissions array when no permissions are set", function() {
-      var manifest = JSON.parse(render("manifest.json", { appPermissions:[] }))
+  describe('#createManifest', function() {
+    it('should have an empty permissions array when no permissions are set', function () {
+      var manifest = JSON.parse(render('manifest.json', { appPermissions:[] }));
       assert.equal(0, manifest.permissions.length);
     });
 
     // These only really test the template generation
-    it("should populate appName.message when appFullname is given", function() {
+    it('should populate appName.message when appFullname is given', function () {
       var data = {
-        appFullName: "Paul1",
-        appDescription: "TEST"
-      }
-      var manifest = JSON.parse(render(path.join("_locales", "en", "messages.json"), data))
-      assert.equal("Paul1", manifest.appName.message);
+        appFullName: 'Paul1',
+        appDescription: 'TEST'
+      };
+      var manifest = JSON.parse(render(path.join('_locales', 'en', 'messages.json'), data));
+      assert.equal('Paul1', manifest.appName.message);
     });
 
-    it("should populate appDescription.message when appDescription is given", function() {
+    it('should populate appDescription.message when appDescription is given', function () {
       var data = {
-        appFullName: "TEST",
-        appDescription: "PauL is Awesome"
-      }
-      var manifest = JSON.parse(render(path.join("_locales", "en", "messages.json"), data))
-      assert.equal("PauL is Awesome", manifest.appDescription.message);
+        appFullName: 'TEST',
+        appDescription: 'PauL is Awesome'
+      };
+      var manifest = JSON.parse(render(path.join('_locales', 'en', 'messages.json'), data));
+      assert.equal('PauL is Awesome', manifest.appDescription.message);
     });
 
-    it("should populate permissions array with 'unlimitedStorage' when 'unlimitedStoraage' is given", function() {
+    it('should populate permissions array with "unlimitedStorage" when "unlimitedStoraage" is given', function () {
       var data = {
-        appPermissions: ["unlimitedStorage"]
-      }
-      var manifest = JSON.parse(render("manifest.json", data));
-      assert.equal(0, manifest.permissions.indexOf("unlimitedStorage"));
+        appPermissions: ['unlimitedStorage']
+      };
+      var manifest = JSON.parse(render('manifest.json', data));
+      assert.equal(0, manifest.permissions.indexOf('unlimitedStorage'));
       assert.equal(1, manifest.permissions.length);
     });
 
-    it("should populate permissions array with 'identity' and 'experimental'  when 'identity' is given", function() {
+    it('should populate permissions array with "identity" and "experimental" when "identity" is given', function () {
       var data = {
-        appPermissions: ["identity", "experimental"]
-      }
-      var manifest = JSON.parse(render("manifest.json", data));
-      assert.notEqual(-1, manifest.permissions.indexOf("identity"));
-      assert.notEqual(-1, manifest.permissions.indexOf("experimental"));
+        appPermissions: ['identity', 'experimental']
+      };
+      var manifest = JSON.parse(render('manifest.json', data));
+      assert.notEqual(-1, manifest.permissions.indexOf('identity'));
+      assert.notEqual(-1, manifest.permissions.indexOf('experimental'));
       assert.equal(2, manifest.permissions.length);
     });
 
-    it("should populate permissions array with 'usb' and 'experimental'  when 'usb' is given", function() {
+    it('should populate permissions array with "usb" and "experimental"  when "usb" is given', function () {
       var data = {
-        appPermissions: ["usb", "experimental"]
-      }
-      var manifest = JSON.parse(render("manifest.json", data));
-      assert.notEqual(-1, manifest.permissions.indexOf("usb"));
-      assert.notEqual(-1, manifest.permissions.indexOf("experimental"));
+        appPermissions: ['usb', 'experimental']
+      };
+      var manifest = JSON.parse(render('manifest.json', data));
+      assert.notEqual(-1, manifest.permissions.indexOf('usb'));
+      assert.notEqual(-1, manifest.permissions.indexOf('experimental'));
       assert.equal(2, manifest.permissions.length);
 
     });
 
-    it("should populate permissions array with 'mediaGalleries' object  when 'mediaGalleries' is given", function() {
+    it('should populate permissions array with "mediaGalleries" object when "mediaGalleries" is given', function () {
       var data = {
-        appPermissions: [ { "mediaGalleries" : ["read", "allAutoDetected"] } ]
-      }
-      var manifest = JSON.parse(render("manifest.json", data));
+        appPermissions: [{ 'mediaGalleries': ['read', 'allAutoDetected'] }]
+      };
+      var manifest = JSON.parse(render('manifest.json', data));
       assert.equal(2, manifest.permissions[0].mediaGalleries.length);
-      assert.notEqual(-1, manifest.permissions[0].mediaGalleries.indexOf("read"));
-      assert.notEqual(-1, manifest.permissions[0].mediaGalleries.indexOf("allAutoDetected"));
+      assert.notEqual(-1, manifest.permissions[0].mediaGalleries.indexOf('read'));
+      assert.notEqual(-1, manifest.permissions[0].mediaGalleries.indexOf('allAutoDetected'));
       assert.equal(1, manifest.permissions.length);
     });
-
  });
 });
